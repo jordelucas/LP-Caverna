56ae13381b01424883150f36b5158ec6dbf130e6
jordelucas <33496399+jordelucas@users.noreply.github.com>
2019-05-28 16:48:01 -0300

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 #include <fstream>
 #include <cstdlib>
 
+#include "Caverna.h"
+
 int main(){
-    
+    Caverna c;
+
+    std::ifstream arq_in;
+
+    arq_in.open("teste.txt");
+
+    c.carregar(arq_in);
+
+    c.toString();
+
+    arq_in.close();
+
+    c.~Caverna();
+    return 0;
 }
